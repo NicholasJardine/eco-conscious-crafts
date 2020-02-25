07479ff5ba66a404affe6ffc612d51058b078552
Nicholas Jardine <nicholasjardine0@gmail.com>
2020-02-25 12:04:26 +0200

    models for products and categories generated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_25_094245) do
+ActiveRecord::Schema.define(version: 2020_02_25_100230) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "title"
+    t.string "photo"
+    t.string "main"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.integer "price"
+    t.string "image"
+    t.string "sub_category_name"
+    t.integer "category_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_products_on_category_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
